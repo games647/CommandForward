85b21690e54913b3039b0b14a72d19fd81eeb504
games647 <games@tsprivat.de>
2016-08-01 21:05:48 +0200

    Added support to remove duplicate commands

--- bungee/src/main/java/com/github/games647/commandforward/bungee/CommandForwardBungee.java
+++ bungee/src/main/java/com/github/games647/commandforward/bungee/CommandForwardBungee.java
@@ -1,7 +1,11 @@
 package com.github.games647.commandforward.bungee;
 
+import com.google.common.cache.CacheBuilder;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
+import java.util.concurrent.ConcurrentMap;
+
+import java.util.concurrent.TimeUnit;
 
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -12,6 +16,13 @@ import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
 public class CommandForwardBungee extends Plugin implements Listener {
+
+    private static final boolean REMOVE_DUPLICATES = false;
+
+    private final ConcurrentMap<String, Object> prevCommands = CacheBuilder.newBuilder()
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .<String, Object>build().asMap();
+
 
     @Override
     public void onEnable() {
@@ -37,13 +48,19 @@ public class CommandForwardBungee extends Plugin implements Listener {
             String arguments = dataInput.readUTF();
 
             ProxiedPlayer invoker = (ProxiedPlayer) messageEvent.getReceiver();
+            String commandLine = command + ' ' + arguments;
+            if (REMOVE_DUPLICATES && prevCommands.containsKey(commandLine)) {
+                return;
+            }
+
+            prevCommands.put(commandLine, new Object());
 
             if (isPlayer) {
                 //the proxied player is the actual invoker other it's the console
-                getProxy().getPluginManager().dispatchCommand(invoker, command + ' ' + arguments);
+                getProxy().getPluginManager().dispatchCommand(invoker, commandLine);
             } else {
                 CommandSender console = getProxy().getConsole();
-                getProxy().getPluginManager().dispatchCommand(console, command + ' ' + arguments);
+                getProxy().getPluginManager().dispatchCommand(console, commandLine);
             }
         }
     }
